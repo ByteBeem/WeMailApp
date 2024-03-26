1aee062a9c6a417c3a6f048ca95d2ced39cacc4a
unknown <202359014@smcsdom.local>
2024-03-26 13:04:52 +0200

    new

--- App/screens/Welcome/welcome.js
+++ App/screens/Welcome/welcome.js
@@ -1,12 +1,57 @@
-import { View, Text, Image } from 'react-native'
+import React, { useeffect, useCallback, useState } from 'react';
+import { View, Text, Alert, ActivityIndicator } from 'react-native'
 import React from 'react'
 import PageContainer from '../components/PageContainer'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import {  SIZES, FONTS } from '../constants'
+import { SIZES, FONTS } from '../constants'
 import Button from '../components/Button'
 import LottieView from 'lottie-react-native';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
 export default function welcome({ navigation }) {
+    const [isLoading, setIsLoading] = useState(false);
+    const navigation = useNavigation();
+    const [appVersion, setAppVersion] = useState('1.0.0');
+
+    const CheckUserStatus = useCallback(async () => {
+        setIsLoading(true);
+        try {
+            const token = await AsyncStorage.getItem("token");
+            if (!token) {
+                Alert.alert("You are not logged in");
+            } else {
+                navigation.navigate("Home");
+            }
+        } catch (error) {
+            console.error("Error fetching user status:", error);
+            Alert.alert("Something went wrong, try again later");
+        } finally {
+            setIsLoading(false);
+        }
+    }, []);
+
+    const CheckAppVersion = useCallback(async () => {
+
+       await axios.get('/api/Appversion')
+            .then(response => {
+                const currentVersion = response.data.version;
+                setAppVersion(currentVersion);
+
+            })
+            .catch(error => {
+                console.error('Error fetching version information:', error);
+            });
+
+
+    }, []);
+
+    useeffect (()=> {
+        CheckAppVersion();
+    }, []);
+
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <PageContainer>
@@ -20,18 +65,18 @@ export default function welcome({ navigation }) {
                     }}
                 >
 
-                    <LottieView source={require("../../assets/json/animation.json")} 
-                    autoPlay
-                    loop
-                    style={{
-                        width: SIZES.width * 0.7,
-                        height: SIZES.width * 0.7,
-                        marginVertical: 48,
-                    }}
+                    <LottieView source={require("../../assets/json/animation.json")}
+                        autoPlay
+                        loop
+                        style={{
+                            width: SIZES.width * 0.7,
+                            height: SIZES.width * 0.7,
+                            marginVertical: 48,
+                        }}
                     />
-                    
 
-                  
+
+
                     <Text
                         style={{
                             ...(SIZES.width <= 360
@@ -57,13 +102,16 @@ export default function welcome({ navigation }) {
 
                         <Button
                             title="launch WeMail"
-                            onPress={() => navigation.navigate('Home')}
+                            onPress={() => CheckUserStatus()}
                             style={{
                                 width: '100%',
                                 paddingVertical: 12,
                                 marginBottom: 48,
                             }}
-                        />
+                        >
+                            {isLoading && <ActivityIndicator size="large" color="#00ff00" />}
+                        </Button>
+
                     </View>
                 </View>
             </PageContainer>
